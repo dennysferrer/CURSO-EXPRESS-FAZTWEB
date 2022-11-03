10c8e7a63cfea25d6cbb9d237d794176292da22d
Dennys Ferrer <dennys.ferrer@gmail.com>
2022-11-02 19:35:47 -0500

    Se trabajo con los diferentes metodos http

--- index.js
+++ index.js
@@ -16,11 +16,26 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.sendFile('./static/index.html', {
-        root: __dirname
-    });
+app.get('/products', (req, res) => {
+    res.send('Lista de productos');
 });
+
+app.post('/products', (req, res) => {
+    res.send('Creando productos');
+});
+
+app.put('/products', (req, res) => {
+    res.send('Actualizando un producto');
+});
+
+app.delete('/products', (req, res) => {
+    res.send('Eliminando un producto');
+});
+
+app.patch('/products', (req, res) => {
+    res.send('Actualizando una parte del producto');
+});
+
 
 app.listen(3000, () => {
     console.log('server listen in port 3000');
